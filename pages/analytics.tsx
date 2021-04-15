4835d5e9c8f3e9c79df1de2b9d2f0c7e2a72b2ee
sjones6 <jones.spencera@gmail.com>
2021-04-14 23:41:01 -0400

    typescript and test pages

---
+++ pages/analytics.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+
+export default function Analytics() {
+  return <>
+    <h1>Analytics</h1>
+    <Link href="/"><a>home</a></Link>
+  </>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to my test!
+        </h1>
+        <div>
+          <Link href="/analytics">analytics</Link>
+        </div>
+      </main>
+    </div>
+  )
+}
